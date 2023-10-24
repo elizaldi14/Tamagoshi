7d2bfd7aaba94e509907eadd4538a7fc1c83e9c7
Alfredo <aelizaldi@ucol.mx>
2023-10-23 19:58:34 -0600

    Obtenemos el name del tamagoshi

--- ejemplo.c
+++ ejemplo.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(){
+/*
+int contador = 0;
 
-printf('Hola papus como estan');
+  for (int i = 0; i < 10; i++) {
+    contador++;
+    printf("contador = %d\n", contador);
+    sleep(10);
+  }
+*/
+
+/*Obtenemos el nombre del tamagoshi*/
+char name[20];
+printf("Hola papu que papu nombre le quieres poner a un tachillomi: ");
+scanf("%s", name);
+
+
 
 return 0;
 
